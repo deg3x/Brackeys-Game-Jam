2df04be5909d5fb0bb34cb3c9cc896049aa2b3af
Konstantinos Benos <mpenos.ks@gmail.com>
2018-08-25 23:20:10 +0300

    Add jump delay and modify collider

--- Brackeys-Game-Jam/Assets/Scripts/PlayerController.cs
+++ Brackeys-Game-Jam/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class PlayerController : MonoBehaviour
 {
-    // Fluid move/jump pair is 6/5
+    // Fluid move/jump pair is 6/5.2
     [Range(4f, 10f)]
     public float movespeed;
     [Range(1f, 10f)]
     public float jumpPower;
+    [Range(0f, 2f)]
+    public float jumpDelay; // Optimal value is around 1.15
 
     private bool isGrounded;
+    private bool canJump;
     private float distToGround;
 
     void Start ()
     {
         distToGround = this.gameObject.GetComponent<Collider>().bounds.extents.y + 0.1f;    // Get the distance to check if object is touching the ground
+        canJump = true;
 
 		if (movespeed == 0f)
         {
@@ -30,12 +34,12 @@ public class PlayerController : MonoBehaviour
 	void FixedUpdate () // Player uses RigidBody so movement in FixedUpdate to be in sync with physics
     {
         HandleMovement();
-	}
+    }
 
     void HandleMovement()
     {
         float x = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(x * movespeed * Time.deltaTime, 0, 0);
+        Vector3 movement = new Vector3(x * movespeed * Time.fixedDeltaTime, 0, 0);
         
         CheckGrounded();
 
@@ -52,24 +56,34 @@ public class PlayerController : MonoBehaviour
         if(Input.GetAxisRaw("Jump") != 0f)
         {
             CheckGrounded();
-            if (isGrounded)
+            if (isGrounded && canJump)
             {
                 Vector3 jumpDir;
                 jumpDir = (move == Vector3.zero) ? Vector3.zero : ((move.x > 0) ? Vector3.right : Vector3.left);    // Looks ugly :)
                 this.gameObject.GetComponent<Rigidbody>().AddForce((jumpDir + Vector3.up) * jumpPower, ForceMode.Impulse);
                 isGrounded = false;
+                canJump = false;
+                Invoke("ResetCanJump", jumpDelay);  // Delay the next jump by jumpDelay seconds, so player cannot spam
             }
         }
     }
 
     void CheckGrounded()
     {
-        Ray r = new Ray(this.transform.position, Vector3.down);
+        Vector3 pos = this.transform.position;
+        float offset = this.gameObject.GetComponent<BoxCollider>().size.x / 2.0f;
+        Ray r1 = new Ray(pos + new Vector3(offset, 0, 0), Vector3.down);
+        Ray r2 = new Ray(pos + new Vector3(-offset, 0, 0), Vector3.down);
 
-        isGrounded = Physics.Raycast(r, distToGround);  // Cast ray downwards to check if we are on the ground
+        isGrounded = (Physics.Raycast(r1, distToGround) || Physics.Raycast(r2, distToGround));  // Cast ray downwards to check if we are on the ground
         if (isGrounded == true)
         {
             this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;  // Zero out the velocity. This solves some jumping bugs 
         }
+    }
+
+    void ResetCanJump()
+    {
+        canJump = true;
     }
 }
