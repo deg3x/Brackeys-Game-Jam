8653c625de4f215076ca32bbfcb4196a2e5d182c
Konstantinos Benos <mpenos.ks@gmail.com>
2018-08-25 14:31:12 +0300

    Add very basic camera controller

--- Brackeys-Game-Jam/Assets/Scripts/CameraController.cs
+++ Brackeys-Game-Jam/Assets/Scripts/CameraController.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = this.transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        this.transform.position = player.transform.position + offset;
+    }
 }
